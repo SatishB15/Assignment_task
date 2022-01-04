977e77fd18cc7fae4744a3573dbc9d73ac910f4d
SatishB15 <satishbirhade1@gmail.com>
2022-01-04 16:08:39 +0530

    Assignment Added

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log('hello!')
+function resizeImage() {
+    let Url = document.getElementById('url').value;
+    let Height = document.getElementById('height').value;
+    let Width = document.getElementById('width').value;
+    let Image = document.getElementById('image');
+    Image.src = Url;
+    Image.height = Height;
+    Image.width = Width;
+}
